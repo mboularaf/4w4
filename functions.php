9e69e1fe99420d99e5bc3590f301ddc76be1c068
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-01-26 11:26:34 -0500

    s1c2 ajout de add_action pour enfiler style.css dans fonction.php

--- functions.php
+++ functions.php
@@ -2,10 +2,12 @@
 /**
  * L'ensemble des fonctions du thème
  */
-
-
-wp_enqueue_style('4w4-gr2-principal', // identificateur
+function enfiler_css(){
+    wp_enqueue_style('4w4-gr1-principal', // identificateur
     get_template_directory_uri() . '/style.css', // adresse url de style.css
     array(), // definir les dépandances
     filemtime(get_template_directory() . '/style.css'),// le calcul de la version du fichier css
 'all'); // média
+add_action( 'wp_enqueue_scripts', 'enfiler_css');
+}
+
