60aa0a5e6b5b569992897ea896686d019c2759fc
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-02-02 11:49:30 -0500

    s2c2 Ajout de front-page.php et single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php 
 /** 
- * index.php est le modèle par défaut du thème 4w4
+ * single.php est le modèle pour un article
 */
 ?>
 <?php 
@@ -11,17 +11,15 @@
 <?php get_header();?>
 
     <main>
-        <pre>front-page.php</pre>
+        <pre>single.php</pre>
         <h1>Bienvenue sur 4W4</h1>
         <?php 
         if (have_posts()):
                 while(have_posts()):
                     the_post();//extrait un objet "post"?>
                     <article>
-                        <a href="<?php the_permalink() ?>"><h3><?php the_title();//afficher le titre du post?></h3></a>
-                        <h6>Extrait:</h6><?php the_excerpt(); ?> 
-                        <h6>Le contenu:</h6><?php the_content();//afficher le contenu ?>
-                        <?php the_permalink() ?>
+                        <h3><?php the_title();//afficher le titre du post?></h3></a> 
+                        <?php the_content();//afficher le contenu ?>
                     </article>
                     <hr>
                 <?php endwhile; ?>
