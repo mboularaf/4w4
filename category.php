84751ca31d8d5c772e3850742cf148a6db88d9c2
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-02-21 13:56:28 -0500

    s5c1 création de la barre de recherche, articles très courts dans category.php, changement de puces dans markdown.scss

--- category.php
+++ category.php
@@ -12,15 +12,14 @@
 <?php get_header();?>
 
     <main>
-        <pre>front-page.php</pre>
+        <pre>category.php</pre>
         <h1>Bienvenue sur 4W4</h1>
         <section class="blocflex">
             <?php if (have_posts()):
                 while(have_posts()):the_post();//extrait un objet "post"?>
                 <article>
-                    <h3><a href="<?php the_permalink() ?>"><?php the_title();//afficher le titre du post?></a></h3>
-                    <?php the_excerpt()?>
-                    <?php wp_trim_words(get_the_excerpt(), 10, "...");?>                      
+                    <h5><a href="<?php the_permalink() ?>"><?php the_title();//afficher le titre du post?></a></h5>
+                    <?= wp_trim_words(get_the_excerpt(), 10, "...");?>                      
                 </article>
             <?php endwhile; ?>
             <?php endif; ?>
