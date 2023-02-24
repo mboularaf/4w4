5e4e873a6c694bfc305c51a3d6950fb6fd6eabed
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-02-23 21:25:04 -0500

    changement de la couleur de fond du main, de la bordure de chaque article,scorrection d'un div dans header.php

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>mon 4w4</title>
     
     
@@ -11,21 +12,18 @@
     <?php wp_head();?>
 </head>
 <body>
-    <header class="site__entete">
+<header class="site__entete">
         <section class="entete__nav">
-            <?php the_custom_logo();?>
-            
-            <div class="navi">
-            <?php wp_nav_menu(array(
-                            "menu"=> "entete",
+            <?php the_custom_logo(); ?> 
+            <?php  wp_nav_menu(array( 
+                            "menu" => "entete",
                             "container" => "nav",
                             "container_class" => "menu__entete"
-        
-                        ));?>
-            <?=get_search_form();?>
-            </div>
+
+                        )); ?>
+            <?= get_search_form(); ?>            
         </section>
-        <h1 class="site__titre"><a  href = "<?= bloginfo('url');?>"><?= bloginfo('name');?> </a></h1>
-        <h2 class="site__description"><?= bloginfo('description');?></h2>
-    </header>
+        <h1 class="site__titre"><a  href="<?= bloginfo('url'); ?>"><?= bloginfo('name'); ?></a></h1>
+        <h2 class="site__description"><?= bloginfo('description'); ?></h2>
+</header>
     
