2b2c443ead36918f91547089fe8a228e3323fa3f
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-03-16 20:49:26 -0400

    s8c2 ajout du width dans body.scss , ajout d'une nouvelle couleur de background header

--- footer.php
+++ footer.php
@@ -1,14 +1,19 @@
 <footer class="site__footer">
+<section>
+        <?php the_custom_logo() ?>
+
+
+        
+</section>
 <?php  wp_nav_menu(array( 
                                 "menu" => "entete",
                                 "container" => "nav",
                                 "container_class" => "menu__entete"
 
                             )); ?>
-                            <span class="material-symbols-outlined">
-mail
-</span>
+                            
 </footer>
         <?php wp_footer(); ?>
+        
 </body>
 </html>    
