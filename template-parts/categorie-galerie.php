484b861f177dfb4ea8e2030b8ac5651e1d5b742e
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-04-13 16:47:20 -0400

    s11c1 Changer les classes des paragraphes pour chacun champ personnalisé

--- template-parts/categorie-galerie.php
+++ template-parts/categorie-galerie.php
@@ -7,5 +7,12 @@
 ?>
 
 <article class="blocflex__galerie">
-    <?php the_content();//le contenu ?>
+    <?php the_content();//le contenu  ?>
+    <p class="reference">
+    <a><?php the_field('reference');?></a>
+    </p>
+    <p class="roulette">
+    <?php the_field('roulette');?>
+    </p>
+
 </article>
