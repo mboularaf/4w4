72b588257a599dff87e1463d5c023fbc5fb11436
Mohamed Boularaf <e1761826@cmaisonneuve.qc.ca>
2023-05-18 16:50:07 -0400

    s14 le footer est adaptatif

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
 */
 ?>
 <footer class="site__footer">
+        <div class="footer_principal">
         <section>
                 <?php the_custom_logo(); ?> 
         </section>
@@ -21,12 +22,15 @@
         <div class="sidebar">
                 <?php dynamic_sidebar( 'footer_2' ); ?>
         </div>
-        </section> 
+        </section>
+        </div> 
+        <div class="footer_social">
         <section class="footer_col">
         <div class="sidebar">
                 <?php dynamic_sidebar( 'footer_3' ); ?>
         </div>
         </section>
+        </div> 
          
         
 
